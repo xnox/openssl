5519d897ce5a42f3eb54c338c02b1cc61f80e483
Dimitri John Ledkov <dimitri.ledkov@surgut.co.uk>
2025-03-06 23:19:25 +0000

    rsa: set verify_message to 1 in rsa_sign when message was digested
    In rsa_sign_init choose to set verify_message indicator to 0, and only
    raise it to 1, once a message has been digested by
    rsa_sign_message_final and no further updates of message digest are
    allowed.
    
    This should correctly mirrror/raise verify-message indicator for
    signing just like for hashing.
    
    Updated existing demo to demonstrate this.

--- demos/signature/rsa_pss_direct.c
+++ demos/signature/rsa_pss_direct.c
@@ -23,8 +23,24 @@
  */
 static const unsigned char test_digest[32] = {0};
 
+/* The data to be signed. This will be hashed. */
+static const unsigned char test_message[] =
+    "This is an example message to be signed.";
+
 /* A property query used for selecting algorithm implementations. */
 static const char *propq = NULL;
+
+void check_verify_message(EVP_PKEY_CTX *ctx) {
+    OSSL_PARAM params[2] = {OSSL_PARAM_END, OSSL_PARAM_END};
+    int verify_message = -1;
+    params[0] = OSSL_PARAM_construct_int(OSSL_SIGNATURE_PARAM_FIPS_VERIFY_MESSAGE, &verify_message);
+    if (!EVP_PKEY_CTX_get_params(ctx, params))
+        printf("xnox failed to get params\n");
+    if (OSSL_PARAM_modified(params))
+        printf("xnox says verify_message is: %d\n", verify_message);
+    else
+        printf("xnox says verify_message is not available\n");
+}
 
 /*
  * This function demonstrates RSA signing of a SHA-256 digest using the PSS
@@ -74,11 +90,6 @@ static int sign(OSSL_LIB_CTX *libctx, unsigned char **sig, size_t *sig_len)
         goto end;
     }
 
-    if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_PSS_PADDING) == 0) {
-        fprintf(stderr, "Failed to configure padding\n");
-        goto end;
-    }
-
     if (EVP_PKEY_CTX_set_signature_md(ctx, md) == 0) {
         fprintf(stderr, "Failed to configure digest type\n");
         goto end;
@@ -104,6 +115,7 @@ static int sign(OSSL_LIB_CTX *libctx, unsigned char **sig, size_t *sig_len)
         fprintf(stderr, "Failed to sign\n");
         goto end;
     }
+    check_verify_message(ctx);
 
     ret = 1;
 end:
@@ -117,69 +129,76 @@ end:
     return ret;
 }
 
-/*
- * This function demonstrates verification of an RSA signature over a SHA-256
- * digest using the PSS signature scheme.
- */
-static int verify(OSSL_LIB_CTX *libctx, const unsigned char *sig, size_t sig_len)
+static int sign_message(OSSL_LIB_CTX *libctx, unsigned char **sig, size_t *sig_len)
 {
     int ret = 0;
-    const unsigned char *ppub_key = NULL;
     EVP_PKEY *pkey = NULL;
     EVP_PKEY_CTX *ctx = NULL;
-    EVP_MD *md = NULL;
+    EVP_SIGNATURE *alg = NULL;
+    const unsigned char *ppriv_key = NULL;
 
-    /* Load DER-encoded RSA public key. */
-    ppub_key = rsa_pub_key;
-    pkey = d2i_PublicKey(EVP_PKEY_RSA, NULL, &ppub_key, sizeof(rsa_pub_key));
+    *sig = NULL;
+
+    /* Load DER-encoded RSA private key. */
+    ppriv_key = rsa_priv_key;
+    pkey = d2i_PrivateKey_ex(EVP_PKEY_RSA, NULL, &ppriv_key,
+                             sizeof(rsa_priv_key), libctx, propq);
     if (pkey == NULL) {
-        fprintf(stderr, "Failed to load public key\n");
+        fprintf(stderr, "Failed to load private key\n");
         goto end;
     }
 
-    /* Fetch hash algorithm we want to use. */
-    md = EVP_MD_fetch(libctx, "SHA256", propq);
-    if (md == NULL) {
-        fprintf(stderr, "Failed to fetch hash algorithm\n");
+    /* Fetch signature algorithm we want to use. */
+    alg = EVP_SIGNATURE_fetch(NULL, "RSA-SHA256", propq);
+    if (alg == NULL) {
+        fprintf(stderr, "Failed to fetch signature algorithm\n");
         goto end;
     }
 
-    /* Create verification context. */
+    /* Create signing context. */
     ctx = EVP_PKEY_CTX_new_from_pkey(libctx, pkey, propq);
     if (ctx == NULL) {
-        fprintf(stderr, "Failed to create verification context\n");
+        fprintf(stderr, "Failed to create signing context\n");
         goto end;
     }
 
-    /* Initialize context for verification and set options. */
-    if (EVP_PKEY_verify_init(ctx) == 0) {
-        fprintf(stderr, "Failed to initialize verification context\n");
+    /* Initialize context for signing and set options. */
+    if (EVP_PKEY_sign_message_init(ctx, alg, NULL) == 0) {
+        fprintf(stderr, "Failed to initialize message signing context\n");
         goto end;
     }
 
-    if (EVP_PKEY_CTX_set_rsa_padding(ctx, RSA_PKCS1_PSS_PADDING) == 0) {
-        fprintf(stderr, "Failed to configure padding\n");
+    /* Determine length of signature. */
+    if (EVP_PKEY_sign(ctx, NULL, sig_len,
+                      test_message, sizeof(test_message)) == 0) {
+        fprintf(stderr, "Failed to get signature length\n");
         goto end;
     }
 
-    if (EVP_PKEY_CTX_set_signature_md(ctx, md) == 0) {
-        fprintf(stderr, "Failed to configure digest type\n");
+    /* Allocate memory for signature. */
+    *sig = OPENSSL_malloc(*sig_len);
+    if (*sig == NULL) {
+        fprintf(stderr, "Failed to allocate memory for signature\n");
         goto end;
     }
 
-    /* Verify signature. */
-    if (EVP_PKEY_verify(ctx, sig, sig_len,
-                        test_digest, sizeof(test_digest)) == 0) {
-        fprintf(stderr, "Failed to verify signature; "
-                "signature may be invalid\n");
+    /* Generate signature. */
+    if (EVP_PKEY_sign(ctx, *sig, sig_len,
+                      test_message, sizeof(test_message)) != 1) {
+        fprintf(stderr, "Failed to sign\n");
         goto end;
     }
+    check_verify_message(ctx);
 
     ret = 1;
 end:
     EVP_PKEY_CTX_free(ctx);
     EVP_PKEY_free(pkey);
-    EVP_MD_free(md);
+    EVP_SIGNATURE_free(alg);
+
+    if (ret == 0)
+        OPENSSL_free(*sig);
+
     return ret;
 }
 
@@ -193,7 +212,7 @@ int main(int argc, char **argv)
     if (sign(libctx, &sig, &sig_len) == 0)
         goto end;
 
-    if (verify(libctx, sig, sig_len) == 0)
+    if (sign_message(libctx, &sig, &sig_len) == 0)
         goto end;
 
     printf("Success\n");
