xnox/openssl

commit a7c0fa601ec29d7a42890aae1e3bccfa0eac896f

Add ifndef to seed-src_jitter too
Reviewed-by: Neil Horman <nhorman@openssl.org>
Reviewed-by: Tomas Mraz <tomas@openssl.org>
Reviewed-by: Paul Dale <ppzgs1@gmail.com>
(Merged from https://github.com/openssl/openssl/pull/25930)

providers/implementations/rands/seed_src_jitter.c
@@ -295,6 +295,7 @@ static size_t jitter_get_seed(void *vseed, unsigned char **pout,
     return ret;
 }
 
+# ifndef OPENSSL_NO_FIPS_JITTER
 size_t ossl_rand_jitter_get_seed(unsigned char **pout, int entropy, size_t min_len, size_t max_len)
 {
     size_t ret = 0;
@@ -310,6 +311,7 @@ size_t ossl_rand_jitter_get_seed(unsigned char **pout, int entropy, size_t min_l
     jitter_free(s);
     return ret;
 }
+# endif
 
 static void jitter_clear_seed(ossl_unused void *vdrbg,
                               unsigned char *out, size_t outlen)

commit 449155f08d7cc1cbefef5fed7e7231bca3c56c49

crypto: fix up OPENSSL_NO_FIPS_JITTER code path
The OPENSSL_NO_FIPS_JITTER codepath is not working correctly, because
the codepaths that rely on such define did not include configuration
header to get the define.

This leads to jitter entropy not being used, when it is expected to be
used.

Fixes: a7c0fa601e (Add ifndef to seed-src_jitter too, 2025-02-23)

crypto/provider_core.c
@@ -8,6 +8,7 @@
  */
 
 #include <assert.h>
+#include <openssl/configuration.h>
 #include <openssl/core.h>
 #include <openssl/core_dispatch.h>
 #include <openssl/core_names.h>

crypto/rand/rand_lib.c
@@ -12,6 +12,7 @@
 
 #include <openssl/err.h>
 #include <openssl/opensslconf.h>
+#include <openssl/configuration.h>
 #include <openssl/core_names.h>
 #include <openssl/provider.h>
 #include "internal/cryptlib.h"

providers/fips/fipsprov.c
@@ -8,6 +8,7 @@
  */
 
 #include <assert.h>
+#include <openssl/configuration.h>
 #include <openssl/core_dispatch.h>
 #include <openssl/core_names.h>
 #include <openssl/params.h>

providers/implementations/rands/seed_src_jitter.c
@@ -9,6 +9,7 @@
 
 #include <string.h>
 #include <openssl/rand.h>
+#include <openssl/configuration.h>
 #include <openssl/core_dispatch.h>
 #include <openssl/e_os2.h>
 #include <openssl/params.h>